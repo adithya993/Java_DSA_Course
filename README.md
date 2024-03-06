3b2fc7ad005bbdcd88c325587bf3617a915ba9e9
adithya993 <adithya.rengarajan993@gmail.com>
2024-03-06 01:06:54 -0800

    Added README.md file

---
+++ README.md
@@ -0,0 +1,3 @@
+Learn Java and DSA here!
+<br>The work is divided into individual chapters for easier understanding.
+<br>Author: Adithya Rengarajan
--- src/main/java/adi/practice/kunalkushwaha/recursion/FilterCharacterInString.java
+++ src/main/java/adi/practice/kunalkushwaha/recursion/FilterCharacterInString.java
@@ -1,47 +1,46 @@
 package adi.practice.kunalkushwaha.recursion;
 
-import org.w3c.dom.ls.LSOutput;
-
 public class FilterCharacterInString {
     public static void main(String[] args) {
         String originalString = "baccappled";
         System.out.println(filterCharacterInString(originalString));
-        System.out.println(skipWordInString(originalString,"apple"));
-        System.out.println(skippartialWordInString(originalString,"app", "apple"));
+        System.out.println(skipWordInString(originalString, "apple"));
+        System.out.println(skippartialWordInString(originalString, "app", "apple"));
     }
 
-    public static String filterCharacterInString(String originalString){
-        if(originalString.isEmpty()){
+    public static String filterCharacterInString(String originalString) {
+        if (originalString.isEmpty()) {
             return "";
         }
         String tmp = "";
-        if(originalString.charAt(0) != 'a'){
+        if (originalString.charAt(0) != 'a') {
             tmp = String.valueOf(originalString.charAt(0));
         }
         return tmp + filterCharacterInString(originalString.substring(1));
     }
 
-    public static String skipWordInString(String originalString, String searchString){
-        if(originalString.isEmpty()){
+    public static String skipWordInString(String originalString, String searchString) {
+        if (originalString.isEmpty()) {
             return "";
         }
 
-        if(originalString.startsWith(searchString)){
-            return skipWordInString(originalString.substring(searchString.length()),searchString);
+        if (originalString.startsWith(searchString)) {
+            return skipWordInString(originalString.substring(searchString.length()), searchString);
         } else {
-            return originalString.charAt(0) + skipWordInString(originalString.substring(1),searchString);
+            return originalString.charAt(0) + skipWordInString(originalString.substring(1), searchString);
         }
     }
 
-    public static String skippartialWordInString(String originalString, String searchString, String skipWord){
-        if(originalString.isEmpty()){
+    public static String skippartialWordInString(String originalString, String searchString, String skipWord) {
+        if (originalString.isEmpty()) {
             return "";
         }
 
-        if(originalString.startsWith(searchString) && !originalString.startsWith(skipWord)){
-            return skippartialWordInString(originalString.substring(searchString.length()),searchString,skipWord);
+        if (originalString.startsWith(searchString) && !originalString.startsWith(skipWord)) {
+            return skippartialWordInString(originalString.substring(searchString.length()), searchString, skipWord);
         } else {
-            return originalString.charAt(0) + skippartialWordInString(originalString.substring(1),searchString,skipWord);
+            return originalString.charAt(0)
+                    + skippartialWordInString(originalString.substring(1), searchString, skipWord);
         }
     }
 }
